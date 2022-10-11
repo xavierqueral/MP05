c10854a8fe800a2edfa1d02a781959b03391030c
xavierqueral <113586138+xavierqueral@users.noreply.github.com>
2022-10-11 20:42:16 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    int numeroAleatorio;
+    srand (time(NULL));
+    numeroAleatorio = (rand() % 6) + 1;
+    printf("numero %i", numeroAleatorio);
+
+    return 0;
+}
